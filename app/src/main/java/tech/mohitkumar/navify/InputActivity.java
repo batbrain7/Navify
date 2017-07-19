1212f5fe7d9965e134345b9f28d6edb395dea4a6
batbrain7 <kumar.mohit983@gmail.com>
2017-07-19 09:09:44 +0530

    Added Input Support

---
+++ app/src/main/java/tech/mohitkumar/navify/InputActivity.java
@@ -0,0 +1,75 @@
+package tech.mohitkumar.navify;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class InputActivity extends AppCompatActivity {
+
+
+    Button button;
+    EditText editText;
+    FloatingActionButton fab;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+
+        editText = (EditText) findViewById(R.id.edit_txt);
+        button = (Button) findViewById(R.id.search_btn);
+        fab = (FloatingActionButton) findViewById(R.id.voice_btn);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                promptSpeechInput();
+            }
+        });
+    }
+
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Speech prompted");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    "Speech not supported",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    Toast.makeText(getApplicationContext(),result.get(0),Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+
+        }
+    }
+}
--- app/src/main/java/tech/mohitkumar/navify/SplashActivity.java
+++ app/src/main/java/tech/mohitkumar/navify/SplashActivity.java
@@ -1,5 +1,6 @@
 package tech.mohitkumar.navify;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Intent intent = new Intent(SplashActivity.this,InputActivity.class);
+        startActivity(intent);
     }
 }
